2d86882da63dee3553b8174f463b7f2c3e119d78
Yurii <yurii.1986.e@gmail.com>
2025-10-07 19:34:20 +0400

    Проверка деплой fix

--- config/urls.py
+++ config/urls.py
@@ -14,13 +14,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
-from rest_framework import permissions
-
-from config import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- users/migrations/0001_initial.py
+++ users/migrations/0001_initial.py
@@ -34,7 +34,8 @@ class Migration(migrations.Migration):
                                                  help_text='Designates whether the user can log into this admin site.',
                                                  verbose_name='staff status')),
                 ('is_active', models.BooleanField(default=True,
-                                                  help_text='Designates whether this user should be treated as active. Unselect this instead of deleting accounts.',
+                                                  help_text='Designates whether this user should be treated as active. '
+                                                            'Unselect this instead of deleting accounts.',
                                                   verbose_name='active')),
                 ('date_joined', models.DateTimeField(default=django.utils.timezone.now, verbose_name='date joined')),
                 ('email', models.EmailField(max_length=254, unique=True, verbose_name='Ваш Email')),
@@ -43,7 +44,8 @@ class Migration(migrations.Migration):
                 ('created_at', models.DateTimeField(auto_now=True, verbose_name='Добавлен')),
                 ('updated_at', models.DateTimeField(auto_now_add=True, verbose_name='Изменён')),
                 ('groups', models.ManyToManyField(blank=True,
-                                                  help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.',
+                                                  help_text='The groups this user belongs to. A user will get all permissions '
+                                                            'granted to each of their groups.',
                                                   related_name='user_set', related_query_name='user', to='auth.group',
                                                   verbose_name='groups')),
                 ('user_permissions', models.ManyToManyField(blank=True, help_text='Specific permissions for this user.',
